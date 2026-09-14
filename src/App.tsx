b29070ccd38608f17f4cf8f4f3b80a88b671b2a6
saitpa <saitpa@kku.ac.th>
2026-09-14 17:59:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+
+// ประกาศตัวแปรไว้ในฟังก์ชันหลักของ Component
+const [isAdmin, setIsAdmin] = useState(false);
+
 import React, { useState, useEffect, useMemo } from 'react';
 import {
   PurchaseRecord,
